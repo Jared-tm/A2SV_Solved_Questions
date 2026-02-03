a97be040da00e672d5b4a63ac66d8fd98f780289
Yared <yaredtamir777@gmail.com>
2026-02-03 15:18:11 +0300

    Added 9. Palindrome Number.py

---
+++ 9. Palindrome Number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        num=x
+        if x<0:
+            return False
+        rev=0
+        while x:
+            rev = rev*10 + x%10
+            x//=10
+        return num==rev
+         
