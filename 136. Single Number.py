f2b0098705fe5a1ea7e5bd6f6b33bfcfff4f56f6
Yared <yaredtamir777@gmail.com>
2026-02-07 19:45:45 +0300

    solved singleNumber problem

---
+++ 136. Single Number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        my_dict={}
+        for num in nums:
+            my_dict[num]=my_dict.get(num,0) +1
+        for k,v in my_dict.items():
+            if v==1:
+                return k 
+
+            
