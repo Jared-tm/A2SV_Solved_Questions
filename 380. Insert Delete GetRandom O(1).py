2a835f8a93da5b0a2f267a432fffbca30d9b889e
Yared <yaredtamir777@gmail.com>
2026-02-12 11:08:41 +0300

    Implemented Randomized Set

---
+++ 380. Insert Delete GetRandom O(1).py
@@ -0,0 +1,38 @@
+class RandomizedSet:
+
+    def __init__(self):
+        self.nums = []
+        self.index_map = {}
+
+    def insert(self, val: int) -> bool:
+        if val in self.index_map:
+            return False
+        
+        self.nums.append(val)
+        self.index_map[val] = len(self.nums) - 1
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.index_map:
+            return False
+
+        index = self.index_map[val]
+        last_element = self.nums[-1]
+
+        self.nums[index] = last_element
+        self.index_map[last_element] = index
+        
+        self.nums.pop()
+        del self.index_map[val]
+        return True
+
+    def getRandom(self) -> int:
+        return random.choice(self.nums)
+        
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
