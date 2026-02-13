90b7aeb201de140497649f61f4170a61ce8b46cd
Yared <yaredtamir777@gmail.com>
2026-02-13 15:29:10 +0300

    Implemented Transpose Matrix

--- 867. Transpose Matrix.py
+++ 867. Transpose Matrix.py
@@ -1,8 +1,9 @@
-matrix = [[1,2,3],[4,5,6]]
-rows, cols = len(matrix) , len(matrix[0])
-mat  = [[0] * rows for i in range(cols)]
-print(mat)
-for i in range(rows):
-    for j in range(cols):
-        mat[j][i] = matrix[i][j]
-print(mat)
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        rows, cols = len(matrix) , len(matrix[0])
+        mat_transpose  = [[0] * rows for i in range(cols)]
+        for i in range(rows):
+            for j in range(cols):
+                mat_transpose[j][i] = matrix[i][j]
+        return mat_transpose
+                
