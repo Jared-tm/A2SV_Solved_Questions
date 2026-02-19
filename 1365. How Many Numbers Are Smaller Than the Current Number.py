060f0716beeccaf9d20b35aa8c7357f1e9cb22d0
Yared <yaredtamir777@gmail.com>
2026-02-19 12:17:15 +0300

    Solved How Many Numbers Are Smaller Than the Current Number

--- 1365. How Many Numbers Are Smaller Than the Current Number.py
+++ 1365. How Many Numbers Are Smaller Than the Current Number.py
@@ -1,14 +1,14 @@
-nums = [8,1,2,2,3]
-res =[]
-pos  = {}
-sorted_nums = sorted(nums)
-unique = set()
-for i in range(len(sorted_nums)):
-    if (sorted_nums[i]) not in unique:
-        unique.add(sorted_nums[i])
-        pos[sorted_nums[i]] = i
-for num in nums:
-    res.append(pos[num])
-print (res)
-
-            
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        res =[]
+        pos  = {}
+        sorted_nums = sorted(nums)
+        unique = set()
+        for i in range(len(sorted_nums)):
+            if (sorted_nums[i]) not in unique:
+                unique.add(sorted_nums[i])
+                pos[sorted_nums[i]] = i
+        for num in nums:
+            res.append(pos[num])
+        return res
+        
