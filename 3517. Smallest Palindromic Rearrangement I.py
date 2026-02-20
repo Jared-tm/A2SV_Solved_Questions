02628e3b0c8c8f0548c12663a5a5a250e54a2cdb
Yared <yaredtamir777@gmail.com>
2026-02-20 09:43:32 +0300

    Solved practice problems

--- 3517. Smallest Palindromic Rearrangement I.py
+++ 3517. Smallest Palindromic Rearrangement I.py
@@ -1,14 +1,12 @@
-s = "aaccd"
-n = len(s)
-s= (sorted(s))
-print(s)
-res = ""
-
-for char in s:
-    res += char
-print(res)
-
-"""if len(res) % 2 ==0:
-    print(res + res[::-1])
-else:
-    print(res+ res[::-1] )"""
+class Solution:
+    def smallestPalindrome(self, s: str) -> str:
+        count = Counter(s)
+        left = [] 
+        middle = ""
+        for char, freq in sorted(count.items()):
+            left.append(char * (freq // 2)) 
+            if freq % 2 == 1:
+                middle = char
+        left = "".join(left) 
+        return left+ middle + left[::-1]
+                
