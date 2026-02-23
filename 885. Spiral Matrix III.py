6fbd89ad5cfaf266ecfaae585014dde60ff4bf08
Yared <yaredtamir777@gmail.com>
2026-02-23 18:17:47 +0300

    Solved Spiral Matrix III

---
+++ 885. Spiral Matrix III.py
@@ -0,0 +1,17 @@
+class Solution:
+    def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
+        direction = [[0,1], [1,0], [0,-1], [-1,0]]
+        res = []
+        step = 1
+        i = 0
+        r, c = rStart , cStart
+        while len(res) < rows * cols: 
+            for x in range(2):
+                dr, dc = direction[i]
+                for y in range(step):
+                    if 0<= r < rows and 0 <= c < cols:
+                        res.append([r,c])
+                    r , c = r + dr, c + dc
+                i = (i + 1)%4
+            step += 1
+        return res
