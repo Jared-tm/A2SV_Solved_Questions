8200512f05db2f5a945a9b59d28a683dbf4845b3
Yared <yaredtamir777@gmail.com>
2026-02-24 14:28:32 +0300

    Solved leetcode 2780

--- 2780. Minimum Index of a Valid Split.py
+++ 2780. Minimum Index of a Valid Split.py
@@ -1,4 +1,3 @@
-from collections import Counter
 class Solution:
     def minimumIndex(self, nums: list[int]) -> int:
         count = {}
@@ -10,25 +9,18 @@ class Solution:
             if 2*value > n:
                 dom  = key
                 break
-            
         total_dom = count[dom]
         k = 0 
         for i in range(n - 1):
             if nums[i] == dom:
                  k += 1
-            
+                 
             left_len = i + 1
             right_len = n - left_len
             
             if k * 2 > left_len and (total_dom - k) * 2 > right_len:
                  return i
-        
         return -1
                  
 
 
-
-
-
-if __name__ == "__main__":
-    print(Solution().minimumIndex([1,2,2,2]))
