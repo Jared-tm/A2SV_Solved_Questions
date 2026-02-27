0f30deb497e3dd70d27150a1679b8ead177fd074
Yared <yaredtamir777@gmail.com>
2026-02-28 00:08:30 +0300

    Solved H-Index

---
+++ 274. H-Index.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        n = len(citations)
+        h = 0
+        for i in range(len(citations)):
+            if citations[i] >= n-i:
+                return n-i
+    
+        return 0
+
+        
