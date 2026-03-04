7a210e8bfd028a483b76695c56a797053a65bb2c
Yared Tamir <110226264+Jared-tm@users.noreply.github.com>
2026-03-04 19:34:06 +0300

    Add solution for Product of Array Except Self

--- leetcode/0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ leetcode/0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -5,21 +5,17 @@ class Solution(object):
         :rtype: List[int]
         """
         n = len(nums)
-        prefix_product = [1]
-        suffix_product = [1]
-        res= []
-
-
+        res= [1]*n
         prod = 1
         for i in range(n):
+            res[i] *= prod
             prod *= nums[i]
-            prefix_product.append(prod)
+            
         prod = 1
         for i in range(n-1,-1,-1):
+            res[i] *= prod
             prod *= nums[i]
-            suffix_product.append(prod)
-        for i in range(n):
-            res.append(prefix_product[i]*suffix_product[n-i-1])
+        
         return res
         
         
