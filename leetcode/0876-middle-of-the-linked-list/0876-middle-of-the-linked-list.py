80c2a176b3bd90df852d538a603e8a24a9c5626a
Yared Tamir <110226264+Jared-tm@users.noreply.github.com>
2026-03-06 17:57:54 +0300

    Add solution for Middle of the Linked List

--- leetcode/0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ leetcode/0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -9,23 +9,10 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        dummy = ListNode(0)
-        dummy.next = head
-
-        curr = dummy
-        size = 0
-        while curr.next:
-            size += 1
-            curr = curr.next
-        
-        tar = (size // 2) + 1
-
-        curr = dummy
-        for i in range(tar):
-            curr = curr.next
-        
-        return curr
-
-        
-
-        
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+                
+        return slow
