9bea792235eb68e750b556680330e6301588e1a9
Yared Tamir <110226264+Jared-tm@users.noreply.github.com>
2026-03-10 18:01:55 +0300

    Add solution for Next Greater Element I

---
+++ leetcode/0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        stack = []
+        next_greater = defaultdict(lambda:-1)
+
+        for num in nums2:
+            while stack and  num > stack[-1]:
+                next_greater[stack.pop()] = num
+            stack.append(num)
+        return [ next_greater[num] for num in nums1]
+
+
+
+        
