8239a5139c797b4d16807101c158393142bd005e
Yared Tamir <110226264+Jared-tm@users.noreply.github.com>
2026-03-13 19:45:28 +0300

    Add solution for Lemonade Change

--- leetcode/0860-lemonade-change/0860-lemonade-change.py
+++ leetcode/0860-lemonade-change/0860-lemonade-change.py
@@ -4,23 +4,23 @@ class Solution(object):
         :type bills: List[int]
         :rtype: bool
         """
-        n = len(bills)
-        #       5, 10
-        changes= [0]*2
-        for i in range(n):
-            if bills[i] == 5:
-                changes[0] += 1
-            elif bills[i] == 10:
-                if changes[0] >= 1:
-                    changes[1] += 1
-                    changes[0] -= 1
+
+        fives = 0
+        tens = 0
+        for bill in bills:
+            if bill == 5:
+                fives += 1
+            elif bill == 10:
+                if fives >= 1:
+                    tens += 1
+                    fives -= 1
                 else: return False
             else:
-                if changes[1] >= 1 and changes[0] >= 1:
-                    changes[1] -= 1
-                    changes[0] -= 1
-                elif changes[0] >= 3:
-                    changes[0] -= 3
+                if tens >= 1 and fives >= 1:
+                    tens -= 1
+                    fives -= 1
+                elif fives >= 3:
+                    fives -= 3
                 else: return False
         return True
 
