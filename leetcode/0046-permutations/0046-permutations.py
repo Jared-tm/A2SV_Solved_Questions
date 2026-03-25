1f361b569d8f7bdac5c80b0df8a2cf9c07777535
Yared Tamir <110226264+Jared-tm@users.noreply.github.com>
2026-03-25 20:53:37 +0300

    Add solution for Permutations

---
+++ leetcode/0046-permutations/0046-permutations.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        used = [False] * len(nums)
+        def backtrack(path):
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+            for i in range(len(nums)):
+                if not used[i]:
+                    used[i] = True
+                    path.append(nums[i])
+                    backtrack(path)
+                    path.pop()
+                    used[i] = False
+        backtrack([])
+        return res
